70efa5541c6f2c3418e4670126e24e20121ee527
simo <mohamedbenizi288@gmail.com>
2023-03-04 16:48:19 +0100

    simo

---
+++ src/main/java/com/fstg/gestioncommertiale/bean/Remboursement.java
@@ -0,0 +1,38 @@
+package com.fstg.gestioncommertiale.bean;
+
+
+
+import javax.persistence.*;
+
+@Entity
+public class Remboursement {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private int code;
+    @OneToOne
+    private AvoirLivraison avoirLivraison;
+
+    public AvoirLivraison getAvoirLivraison() {
+        return avoirLivraison;
+    }
+
+    public void setAvoirLivraison(AvoirLivraison avoirLivraison) {
+        this.avoirLivraison = avoirLivraison;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
---
+++ src/main/java/com/fstg/gestioncommertiale/bean/ReçuRemboursement.java
@@ -0,0 +1,27 @@
+package com.fstg.gestioncommertiale.bean;
+
+import javax.persistence.*;
+
+@Entity
+public class ReçuRemboursement {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @OneToOne
+    private Remboursement remboursement;
+
+    public Remboursement getRemboursement() {
+        return remboursement;
+    }
+
+    public void setRemboursement(Remboursement remboursement) {
+        this.remboursement = remboursement;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
