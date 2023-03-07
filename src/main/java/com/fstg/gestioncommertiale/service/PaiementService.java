0870fc1e03818ca59df5ef8078dfab2bd7198f30
ORIGINAL SHOP <arkonteoumnia@gmail.com>
2023-03-07 22:50:18 +0100

    first commit

---
+++ src/main/java/com/fstg/gestioncommertiale/service/PaiementService.java
@@ -0,0 +1,36 @@
+package com.fstg.gestioncommertiale.service;
+
+import com.fstg.gestioncommertiale.bean.Paiement;
+import com.fstg.gestioncommertiale.dao.PaiementDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class PaiementService {
+    @Autowired
+    private PaiementDao paiementDao;
+
+    public Paiement findByCode(String code) {
+        return paiementDao.findByCode(code);
+    }
+@Transactional
+    public int deleteByCode(String code) {
+        return paiementDao.deleteByCode(code);
+    }
+
+    public List<Paiement> findAll() {
+        return paiementDao.findAll();
+    }
+
+    public int save(Paiement paiement) {
+        if (findByCode(paiement.getCode())!= null){
+            return -1 ;
+        }else {
+            paiementDao.save(paiement);
+            return 1 ;
+        }
+    }
+}
