6d8d544ce29725e0c2a68ecf75427de3a26d7765
simo <mohamedbenizi288@gmail.com>
2023-03-08 21:59:21 +0100

    simo

--- src/main/java/com/fstg/gestioncommertiale/dao/RemboursementDao.java
+++ src/main/java/com/fstg/gestioncommertiale/dao/RemboursementDao.java
@@ -2,7 +2,9 @@ package com.fstg.gestioncommertiale.dao;
 
 import com.fstg.gestioncommertiale.bean.Remboursement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RemboursementDao extends JpaRepository<Remboursement, Long> {
 
     Remboursement findByCode(String code);
