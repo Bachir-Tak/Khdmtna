6a53c2801738fb6923aee5d906fe43ca1f22af58
Yazoo <yahyaaitbrahim01@gmail.com>
2023-03-08 10:31:20 +0100

    yahay

--- src/main/java/com/fstg/gestioncommertiale/service/AvoirLivraisonService.java
+++ src/main/java/com/fstg/gestioncommertiale/service/AvoirLivraisonService.java
@@ -1,5 +1,6 @@
 package com.fstg.gestioncommertiale.service;
 
+import com.fstg.gestioncommertiale.bean.AvoirLivraison;
 import com.fstg.gestioncommertiale.dao.AvoirLivraisonDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,21 @@ import org.springframework.stereotype.Service;
 public class AvoirLivraisonService {
     @Autowired
     private AvoirLivraisonDao avoirLivraisonDao;
+
+    public AvoirLivraison findByCode(String code) {
+        return avoirLivraisonDao.findByCode(code);
+    }
+
+    public int deleteByCode(String code) {
+        return avoirLivraisonDao.deleteByCode(code);
+    }
+    public boolean save(AvoirLivraison avoirLivraison) {
+        AvoirLivraison existingAvoirLivraison = avoirLivraisonDao.findByCode(avoirLivraison.getCode());
+        if (existingAvoirLivraison != null) {
+            return false;
+        } else {
+            avoirLivraisonDao.save(avoirLivraison);
+            return true;
+        }
+    }
 }
--- src/main/java/com/fstg/gestioncommertiale/service/LivraisonCommandeService.java
+++ src/main/java/com/fstg/gestioncommertiale/service/LivraisonCommandeService.java
@@ -1,11 +1,46 @@
 package com.fstg.gestioncommertiale.service;
 
+import com.fstg.gestioncommertiale.bean.LivraisonCommande;
 import com.fstg.gestioncommertiale.dao.LivraisonCommandeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LivraisonCommandeService {
     @Autowired
     private LivraisonCommandeDao livraisonCommandeDao;
+
+    public List<LivraisonCommande> findByLivraisonRef(String ref) {
+        return livraisonCommandeDao.findByLivraisonRef(ref);
+    }
+
+    public int deleteByLivraisonRef(String ref) {
+        return livraisonCommandeDao.deleteByLivraisonRef(ref);
+    }
+
+    public List<LivraisonCommande> findByCommandeRef(String ref) {
+        return livraisonCommandeDao.findByCommandeRef(ref);
+    }
+
+    public int deleteByCommandeRef(String ref) {
+        return livraisonCommandeDao.deleteByCommandeRef(ref);
+    }
+
+    public List<LivraisonCommande> findByLivraisonId(Long id) {
+        return livraisonCommandeDao.findByLivraisonId(id);
+    }
+
+    public int deleteByLivraisonId(Long id) {
+        return livraisonCommandeDao.deleteByLivraisonId(id);
+    }
+
+    public List<LivraisonCommande> findByCommandeId(Long id) {
+        return livraisonCommandeDao.findByCommandeId(id);
+    }
+
+    public int deleteByCommandeId(Long id) {
+        return livraisonCommandeDao.deleteByCommandeId(id);
+    }
 }
