d5949ecd0cfd7b53d999cc389535d939378c8063
Fathallah <fathallah.amezouar@edu.uca.ma>
2023-03-08 23:52:10 +0100

    commit fathallah

--- src/main/java/com/fstg/gestioncommertiale/ws/CategorieProduitProvided.java
+++ src/main/java/com/fstg/gestioncommertiale/ws/CategorieProduitProvided.java
@@ -1,9 +1,13 @@
 package com.fstg.gestioncommertiale.ws;
 
+import com.fstg.gestioncommertiale.bean.CategorieProduit;
 import com.fstg.gestioncommertiale.service.CategorieProduitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @RestController
 @RequestMapping
@@ -11,4 +15,20 @@ public class CategorieProduitProvided {
     @Autowired
     private CategorieProduitService categorieProduitService;
 
+    public CategorieProduit findByCode(String code) {
+        return categorieProduitService.findByCode(code);
+    }
+
+    public int save(CategorieProduit categorieProduit) {
+        return categorieProduitService.save(categorieProduit);
+    }
+
+    @Transactional
+    public int deleteByCode(String code) {
+        return categorieProduitService.deleteByCode(code);
+    }
+
+    public List<CategorieProduit> findAll() {
+        return categorieProduitService.findAll();
+    }
 }
